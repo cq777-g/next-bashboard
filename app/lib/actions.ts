a0020f5fff20b3e0f68ac2c3b665d9f6a0874b74
cq <969685444@qq.com>
2023-11-09 13:09:42 +0800

    删除发票

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,3 +60,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+// 删除发票
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
