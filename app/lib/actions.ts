8015dc93caba6e1927ea94837435f648865dc7a2
cq <969685444@qq.com>
2023-11-09 14:51:41 +0800

    添加用户登录认证

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 // 添加校验
 const InvoiceSchema = z.object({
@@ -122,4 +123,18 @@ export async function deleteInvoice(id: string) {
         return { message: 'Database Error: Failed to Delete Invoice.' }
     }
 
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', Object.fromEntries(formData));
+    } catch (error) {
+        if ((error as Error).message.includes('CredentialsSignin')) {
+            return 'CredentialSignin';
+        }
+        throw error;
+    }
 }
